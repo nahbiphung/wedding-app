48ae3721a26fea0f3a79825f02556225f843ff83
LAPTOP-DTGG27J2\Admin <phunggiaoai@gmail.com>
2025-06-04 20:51:37 +0700

    clean up

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,12 +1,4 @@
 export const environment = {
     production: false,
-    firebase: {
-        apiKey: 'AIzaSyDY4sKv5JE_qGvuMdnZQW_92km8HAn4MEE',
-        authDomain: 'oai-test-3d517.firebaseapp.com',
-        projectId: 'oai-test-3d517',
-        storageBucket: 'oai-test-3d517.firebasestorage.app',
-        messagingSenderId: '1003260488002',
-        appId: '1:1003260488002:web:4a0924195e6fdccf5dc0db',
-        measurementId: 'G-DP4E14CSB2',
-    },
+    firebase: {},
 };
